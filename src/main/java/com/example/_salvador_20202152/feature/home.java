3732b772b36d18a819b62392ad40e260b133b2a6
AnaisSalvador <104086322+AnaisSalvador@users.noreply.github.com>
2023-08-24 21:51:49 -0500

    commit2

--- src/main/java/com/example/_salvador_20202152/feature/home.java
+++ src/main/java/com/example/_salvador_20202152/feature/home.java
@@ -1,5 +1,10 @@
 package com.example._salvador_20202152.feature;
 import com.example._salvador_20202152.entity.persona;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feature")
 public class home {
 
 }
--- src/main/java/com/example/_salvador_20202152/feature/navbar.java
+++ src/main/java/com/example/_salvador_20202152/feature/navbar.java
@@ -1,4 +1,23 @@
 package com.example._salvador_20202152.feature;
+import com.example._salvador_20202152.entity.persona;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/feature/navbar")
 public class navbar {
+    @GetMapping(value=" ")
+    public String vuelos(){
+        return "/feature/navbar/vuelo";
+    }
+
+    @GetMapping("/buscar")
+    public String buscar(@RequestParam(name = "vuelo", required = false) String vue,
+                         @RequestParam(name = "paquete", required = false) String paquete){
+        System.out.println("vuelo: " + vue);
+        System.out.println("paquete:" + paquete);
+        return "feature/vuelos";
+    }
 }
